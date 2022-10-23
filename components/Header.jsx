4d3541cc284158ca60f5bb3fae88a791cfc55c9a
Umair Mirza <umair_hp@hotmail.com>
2022-10-23 17:38:47 +0500

    Added navigation bar

---
+++ components/Header.jsx
@@ -0,0 +1,38 @@
+import React, {useContext} from 'react'
+import Link from 'next/link'
+
+const categories = [
+    {name: "Web Development", slug: "web-development"},
+    {name: "Web Design", slug: "web-design"},
+    {name: "Frontend", slug: "frontend"},
+    {name: "Styling", slug: "styling"},
+]
+
+const Header = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+        <div className="border-b w-full inline-block border-blue-400 py-8">
+            <div className="md:float-left block">
+                <Link href="/">
+                    <span className="cursor-pointer font-bold text-4xl text-white">
+                        Next Blog
+                    </span>
+                </Link>
+            </div>
+            <div className="hidden md:float-left md:contents">
+                {
+                    categories.map((category, index) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
