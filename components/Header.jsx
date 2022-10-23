59eb43ef3d5678c6cd7974e4e24c97d1a5f2926d
Umair Mirza <umair_hp@hotmail.com>
2022-10-24 02:02:37 +0500

    Implemented the Categories widget and modified Header to use it

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,17 @@
-import React, {useContext} from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
 
-const categories = [
-    {name: "Web Development", slug: "web-development"},
-    {name: "Web Design", slug: "web-design"},
-    {name: "Frontend", slug: "frontend"},
-    {name: "Styling", slug: "styling"},
-]
+import { getCategories } from '../services'
 
 const Header = () => {
+    const[categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories()
+        .then((newCategories) => setCategories(newCategories))
+
+    }, [])
+
   return (
     <div className="container mx-auto px-10 mb-8">
         <div className="border-b w-full inline-block border-blue-400 py-8">
