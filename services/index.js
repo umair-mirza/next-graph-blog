1a81511728a8b9a25cdd98a8be58ad5bb88b6973
Umair Mirza <umair_hp@hotmail.com>
2022-10-24 14:16:34 +0500

    finalized Post detail with the raw content

--- services/index.js
+++ services/index.js
@@ -60,7 +60,7 @@ export const getRecentPosts = async () => {
   return result.posts
 }
 
-export const getSimilarPosts = async() => {
+export const getSimilarPosts = async(categories, slug) => {
   //don't get the current article but get articles belonging to other categories
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
@@ -78,7 +78,7 @@ export const getSimilarPosts = async() => {
     }
   `
 
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, {categories, slug})
     
   return result.posts
 }
@@ -96,4 +96,39 @@ export const getCategories = async () => {
   const result = await request(graphqlAPI, query)
     
   return result.categories
+}
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+  query GetPostDetails($slug: String!) {
+    post(where: {slug: $slug}) {
+      author {
+        bio
+        name
+        id
+        photo {
+          url
+        }
+      }
+      createdAt
+      slug
+      title
+      excerpt
+      featuredImage {
+        url
+      }
+      categories {
+        name
+        slug
+      }
+      content {
+        raw
+      }
+    }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, {slug})
+  
+  return result.post
 }
